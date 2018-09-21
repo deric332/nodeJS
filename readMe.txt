f2ac945a14ca5aca46dd9d89953f27b4b1cadd92
Deric Pavoo <deric.akp@gmail.com>
2018-09-21 18:36:31 +0530

    S3 Upload succesful with readme

---
+++ readMe.txt
@@ -0,0 +1,13 @@
+-- AWS CLI need to be installed on your machine or server
+-- Use command AWS CONFIGURE to update the accesskey and secretaccess key
+-- You can verify the file created in the location C:\Users\deric.pavoo\.aws in windows
+-- config file should have location deatils
+        [default]
+        region = ap-south-1
+-- Credentials file should have AWS keys
+        [default]
+        aws_access_key_id = AKIAISFEZZWERY7VXDNQ
+        aws_secret_access_key = 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
+-- npm install aws-sdk
+-- npm install node_modules
+-- npm install express
--- server/app.js
+++ server/app.js
@@ -1,11 +1,27 @@
 var express     =require('express');
 var myfun       = require('./func'); // not identifying the module
-var aws         = require(''aws-sdk);
+var aws         = require('aws-sdk');
 var app=express();
 
-
-app.route('/authenticate').get(function (req,res) {
+var s3 = new aws.S3({region: 'ap-south-1'});
+    app.route('/authenticate').get(function (req,res) {
     myfun.doHash();
+        var myBucket = 'deric333';
+        var myKey = 'myBucketKey';
+        s3.createBucket({Bucket: myBucket}, function(err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                params = {Bucket: myBucket, Key: myKey, Body: 'Hello!'};
+                s3.putObject(params, function(err, data) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log("Successfully uploaded data to myBucket/myKey");
+                    }
+                });
+            }
+        });
     res.send("Get Node Router");
 });
 app.route('/authenticate').post(function (req,res) {
